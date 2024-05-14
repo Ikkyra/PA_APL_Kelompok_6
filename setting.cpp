2b971c8bd5ebc5bcd2984efb13dc3b8471986d8b
Andhika <18984@sman1samarinda.sch.id>
2024-05-14 20:31:41 +0800

    setting

---
+++ setting.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct settings{
+    int pomodoro;
+    int short_break;
+    int long_break;
+};
+
+int main(){
+    string pili;
+    cout<<"[1] Setting Pomodoro\n";    
+    cout<<"[2] Setting Short Break\n";    
+    cout<<"[3] Setting Long Break\n";    
+    cout<<"[4] Setting Long Break Interval\n";
+    cout<<"[5] Kembali\n";
+    cout<<"Masukkan Pilihan Anda: ";cin>>pili;
+    if (pili=="1"){
+        int baru;
+        cout<<"Masukkan Waktu Pomodoro Baru: ";cin>>baru;
+        pomodoro[n][0] = baru;
+    }    
+    else if (pili=="1"){
+
+    }    
+    else if (pili=="2"){
+
+    }    
+    else if (pili=="3"){
+
+    }    
+    else if (pili=="4"){
+
+    }    
+    else if (pili=="5"){
+
+    }    
+    else{
+        cout<<"Pilihan Tidak Valid";
+    }    
+}
