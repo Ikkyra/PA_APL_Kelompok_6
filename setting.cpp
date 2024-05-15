793cef95104395a087ca985e0069b0e652cbfadb
Andhika <18984@sman1samarinda.sch.id>
2024-05-15 21:29:23 +0800

    -1 = Done

--- setting.cpp
+++ setting.cpp
@@ -1,69 +1,169 @@
 #include <iostream>
-#include "main.cpp"
-using namespace std;
+#include <fstream>
+#include <sstream>
+#include <string>
 
 struct account {
     int id;
-    string username;
-    string password;
+    std::string username;
+    std::string password;
 };
 
-struct settings{
+struct settings {
     int pomodoro;
     int short_break;
     int long_break;
     int long_break_interval;
 };
 
+account akun[100];
+settings sett[100];
 
+std::fstream file;
 
-int main(){
-    string pili;
-    account akun[100];
-    settings sett[100];
-    int y;
-    for (int i = 0; i < userCount; ++i) {
-        if (usernames[i] == username && passwords[i] == password) {
-            y = i ;
+int countLines(const std::string& filename) {
+    std::ifstream file(filename);
+    int lines = 0;
+    std::string line;
+    while (std::getline(file, line)) {
+        ++lines;
+    }
+    file.close();
+    return lines - 1; 
+}
+
+void readCSV(const std::string& filename, settings* sett, int size) {
+    std::ifstream file(filename);
+    std::string line;
+
+    if (!file.is_open()) {
+        std::cerr << "Gagal Membuka File" << std::endl;
+        return;
+    }
+
+    std::getline(file, line);
+
+    int index = 0;
+    while (std::getline(file, line) && index < size) {
+        std::stringstream ss(line);
+        std::string value;
+
+        try {
+            std::getline(ss, value, ',');
+            sett[index].pomodoro = std::stoi(value);
+
+            std::getline(ss, value, ',');
+            sett[index].short_break = std::stoi(value);
+
+            std::getline(ss, value, ',');
+            sett[index].long_break = std::stoi(value);
+
+            std::getline(ss, value, ',');
+            sett[index].long_break_interval = std::stoi(value);
+
+            ++index;
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Invalid data format : " << line << std::endl;
+        } catch (const std::out_of_range& e) {
+            std::cerr << "Number out of range : " << line << std::endl;
         }
     }
-    cout<<"[1] Setting Pomodoro\n";    
-    cout<<"[2] Setting Short Break\n";    
-    cout<<"[3] Setting Long Break\n";    
-    cout<<"[4] Setting Long Break Interval\n";
-    cout<<"[5] Kembali\n";
-    cout<<"Masukkan Pilihan Anda: ";cin>>pili;
-    if (pili=="1"){
-        int baru1;
-        int n = akun[y].id ;
-        cout<<"Masukkan Waktu Pomodoro Baru: ";cin>>baru1;
-        sett[y].pomodoro = baru1;
-    }   
-    else if (pili=="2"){
-        int baru2;
-        int n = akun[y].id ;
-        cout<<"Masukkan Waktu Short Break Baru: ";cin>>baru2;
-        sett[y].short_break = baru2;
 
-    }    
-    else if (pili=="3"){
-        int baru3;
-        int n = akun[y].id ;
-        cout<<"Masukkan Waktu Long Break Baru: ";cin>>baru3;
-        sett[y].long_break = baru3;
+    file.close();
+}
 
-    }    
-    else if (pili=="4"){
-        int baru4;
-        int n = akun[y].id ;
-        cout<<"Masukkan Interval Long Break Baru: ";cin>>baru4;
-        sett[y].long_break_interval = baru4;
+void tuliscsv(const std::string& filename, const settings* sett, int size) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Gagal Membuka File" << std::endl;
+        return;
+    }
 
-    }     
-    else if (pili=="5"){
-        return ;
-    }    
-    else{
-        cout<<"Pilihan Tidak Valid";
-    }    
+    file << "Pomodoro, Short Break, Long Break, Long Break Interval\n"; // Menulis header
+
+    for (int i = 0; i < size; ++i) {
+        file << sett[i].pomodoro << ", " << sett[i].short_break << ", "
+             << sett[i].long_break << ", " << sett[i].long_break_interval << "\n";
+    }
+
+    file.close();
+}
+
+int main() {
+    std::string filename = "dataWaktu.csv";
+    int lineCount = countLines(filename);
+    
+    if (lineCount <= 0) {
+        std::cerr << "File Kosong" << std::endl;
+        return 1;
+    }
+
+    settings* settingsArray = new (std::nothrow) settings[lineCount];
+    if (!settingsArray) {
+        std::cerr << "Gagal Mengalokasikan Memory" << std::endl;
+        return 1;
+    }
+    readCSV(filename, settingsArray, lineCount);
+
+    std::string pili;
+    int y = 0;  
+    while (true) {
+        std::cout << "-------------------------------\n";    
+        std::cout << "|          SETTINGS           |\n";    
+        std::cout << "===============================\n";    
+        std::cout << "[1] Setting Pomodoro\n";    
+        std::cout << "[2] Setting Short Break\n";    
+        std::cout << "[3] Setting Long Break\n";    
+        std::cout << "[4] Setting Long Break Interval\n";
+        std::cout << "[5] Kembali\n";
+        std::cout << "===============================\n";    
+        std::cout << "Masukkan Pilihan Anda: "; std::cin >> pili;
+
+        int newValue;
+        if (pili >= "1" && pili <= "4") {
+            try {
+                while (true) {
+                    std::cout << "\nMasukkan Nilai Baru: ";
+                    if (!(std::cin >> newValue)) {
+                        std::cout << "Error: Harap Masukkan Dalam Bentuk Angka!!" << std::endl;
+                        std::cin.clear();
+                        std::cin.ignore();
+                        continue;
+                    }
+
+                    if (newValue < 1) {
+                        std::cout << "Error: Nilai Tidak Boleh Kurang dari 1" << std::endl;
+                        continue;
+                    } else {
+                        switch(pili[0]) {
+                            case '1':
+                                sett[y].pomodoro = newValue;
+                                break;
+                            case '2':
+                                sett[y].short_break = newValue;
+                                break;
+                            case '3':
+                                sett[y].long_break = newValue;
+                                break;
+                            case '4':
+                                sett[y].long_break_interval = newValue;
+                                break;
+                        }
+                        throw newValue;
+                    }
+                }
+            } catch (int a) {
+                std::cout << "Nilai berhasil diperbarui menjadi: " << a << std::endl;
+            }
+        } else if (pili == "5") {
+            tuliscsv(filename, sett, lineCount);
+            break;
+        } else {
+            std::cout << "Pilihan Tidak Valid" << std::endl;
+        }
+    }
+
+    delete[] settingsArray;
+
+    return 0;
 }
